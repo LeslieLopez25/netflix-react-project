66978aacbec210a0056177a525d39a195370bd93
Leslie Lopez <leslie2515lopez@gmail.com>
2023-03-08 00:57:03 -0600

    Row component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import Main from "../components/main";
+import Row from "../components/row";
+import requests from "../Request";
 
-const home = () => {
-  return <Main />;
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <Row title="UpComing" fetchURL={requests.requestUpcoming} />
+      <Row title="Popular" fetchURL={requests.requestPopular} />
+      <Row title="Trending" fetchURL={requests.requestTrending} />
+      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row title="Horror" fetchURL={requests.requestHorror} />
+    </>
+  );
 };
 
-export default home;
+export default Home;
