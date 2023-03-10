b04add356867545d683d2c02bee50176f6987c58
Leslie Lopez <leslie2515lopez@gmail.com>
2023-03-09 23:01:30 -0600

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/protectedRoute";
+import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/account";
 import Home from "./pages/home";
+import Login from "./pages/login";
+import SignUp from "./pages/signUp";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
     </>
   );
 }
